48cbdd0b06204166ebd3d596d7fa1abc07bd3abb
astrocoding <zaenalalfian20@gmail.com>
2025-12-13 17:22:44 +0700

    feats(pertemuan6)

---
+++ pertemuan6/index.js
@@ -0,0 +1,17 @@
+let angka1;
+angka1 = "Nama saya";
+console.log(angka1);
+angka1 = 100;
+angka1 = true;
+console.log(angka1);
+
+let nama = "Zaenal"; // ES6 = EcmaScript 6
+var fullName = "Alfian";
+function name() {
+  // nama = "ZEN";
+  fullName = "Zaenal Alfian";
+  console.log(fullName);
+}
+console.log(nama);
+console.log(fullName);
+name();
